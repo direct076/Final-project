79e0534e316f1f37ec67cdf8db8f11d14a6456fe
school <school@A45PC6>
2025-04-01 09:51:14 +0300

    save\load

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -14,19 +14,24 @@
 		- певну кількість літрів
 	замовлення кави
  */
+using System.Diagnostics;
+using System.Text.Json;
+
 Console.OutputEncoding = System.Text.Encoding.UTF8; // для укр. мови
 Console.WriteLine("---------- Welcome to Gas Staton BDCM ----------");
 
 Console.WriteLine("\tMENU:");
 Console.WriteLine("\t1. Refill the petrol station with the appropriate type of fuel");
 Console.WriteLine("\t2. View available fuel of each type");
-Console.WriteLine("\t3. Change the price of fuel");
-Console.WriteLine("\t4. Refuelling, a coffee machine");
-Console.WriteLine("\t5. Cache withdrawal (for fuel, for coffee)");
-Console.WriteLine("\t6. Display of statistics on fuel sales in litres and UAH.");
-Console.WriteLine("\t7. Withdrawal of earned funds (each category separately and in total)");
+Console.WriteLine("\t3. Save to file");
+Console.WriteLine("\t4. Load from file");
+Console.WriteLine("\t5. Change the price of fuel");
+Console.WriteLine("\t6. Refuelling, a coffee machine");
+Console.WriteLine("\t7. Cache withdrawal (for fuel, for coffee)");
+Console.WriteLine("\t8. Display of statistics on fuel sales in litres and UAH.");
+Console.WriteLine("\t9. Withdrawal of earned funds (each category separately and in total)");
 
-Fuel fl = new();
+List<Fuel> fuels = new();
 
 while (true)
 {
@@ -36,23 +41,48 @@ while (true)
     switch (choice)
     {
         case 1:
-            Console.Write("Enter product name: ");
-            fl.Type = Console.ReadLine();
-            Console.Write("Enter product price: ");
-            fl.Price = Convert.ToDouble(Console.ReadLine());
+            var newItem = new Fuel();
+
+            Console.Write("Enter fuel type name: ");
+            newItem.Type = Console.ReadLine();
+            Console.Write("Enter fuel price: ");
+            newItem.Price = Convert.ToDouble(Console.ReadLine());
             Console.Write("Enter product volume: ");
-            fl.Volume = Convert.ToInt32(Console.ReadLine());
+            newItem.Volume = Convert.ToInt32(Console.ReadLine());
+           
+
+            fuels.Add(newItem);
             break;
 
 
         case 2:
-            Console.Write("Petlor");
-            Console.Write("Gas");
-            Console.Write("Disel");
-            Console.Write("Bio-Disel");
-            
+            foreach (var item in fuels)
+            {
+                Console.WriteLine("------- fuel ---------");
+                Console.WriteLine($":Type {item.Type}");
+                Console.WriteLine($"Volume: {item.Volume}");
+                Console.WriteLine($"Price: {item.Price}");
+
+            }
+
             break;
+
+
+        case 3:
+            string jsonToSave = JsonSerializer.Serialize(fuels);
+            Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+            File.WriteAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/products_db.json", jsonToSave);
+            break;
+        case 4:
+            string jsonToLoad = File.ReadAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/products_db.json");
+            fuels = JsonSerializer.Deserialize<List<Fuel>>(jsonToLoad);
+            break;
+
+       
+           
     }
+
+     
 }
 public class Fuel
 {
